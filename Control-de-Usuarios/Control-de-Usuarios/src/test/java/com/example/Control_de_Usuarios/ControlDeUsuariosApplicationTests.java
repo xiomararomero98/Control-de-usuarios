82b5790c29b027092620a98d1b1df5fe8de3801c
xiomararomero98 <xio.romero@duocuc.cl>
2025-05-14 16:50:43 -0400

    control de usuarios
    control de usuarios

---
+++ Control-de-Usuarios/Control-de-Usuarios/src/test/java/com/example/Control_de_Usuarios/ControlDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Control_de_Usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
